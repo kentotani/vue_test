79d061be6663303e9d9c9823425b1d78b6b64f1d
kentotani <kento.tani@seattleconsulting.co.jp>
2018-10-04 21:06:02 +0900

    apolloOK

---
+++ app/javascript/packs/components/app.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <header>
+      <h1>Task</h1>
+    </header>
+    <hr>
+    <main>
+      <lane_add></lane_add>
+        <p>{{ user }}</p>
+    </main>
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable';
+import lane_add from './lane_add'
+import gql from 'graphql-tag'
+
+const FeedQuery = gql`
+{
+  user {
+    id
+    name
+  }
+}
+`
+export default {
+  name: 'App',
+  components: {
+    draggable,
+    lane_add,
+  },
+  data() {
+    return {
+      user: "", //レスポンスを入れるプロパティの初期化
+    }
+  },
+  apollo: {
+    user: {
+      query: FeedQuery,
+      loadingKey: 'loading',
+    },
+  },
+}
+</script>
+
+<style>
+</style>
--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue/dist/vue.esm.js'
-import Aaa from './components/aaa.vue'
+import App from './components/app.vue'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+
+Vue.config.productionTip = false
+
+// install the vue-momnet plugin
+Vue.use(require('vue-moment'))
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: new ApolloClient({
+    link: new HttpLink({ uri: 'http://localhost:3000/graphql' }),
+    cache: new InMemoryCache()
+  })
+})
 
 new Vue({
   el: '#example',
-  components: { Aaa },
-  template: '<Aaa/>'
+  provide: apolloProvider.provide(),
+  components: { App },
+  template: '<App/>'
 })
